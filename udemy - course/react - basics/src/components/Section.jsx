c2d8b6d82ca4809a554759a95c0a27b7c686a817
Tóth Attila <tothattila5559@gmail.com>
2025-09-26 14:15:55 +0200

    deleted files

--- udemy - course/react - basics/src/components/Section.jsx
+++
@@ -1,8 +0,0 @@
-export default function Section({ title, children, ...props }) {
-    {/* ...props -> merge the rest of the data, into the props object, that was not manually extracted
-        ... -> spread data on to some element*/}
-    return <section { ...props }>
-        <h2>{ title }</h2>
-        { children }
-    </section>
-}
